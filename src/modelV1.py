236f56d894024f5c78c86fee3d4b7d11d8e6d548
Gina Santana <endernova@Ginas-MacBook-Pro.local>
2020-09-14 19:54:40 -0600

    Update model code

--- src/modelV1.py
+++ src/modelV1.py
@@ -6,12 +6,19 @@ from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
+from sklearn.metrics import plot_confusion_matrix
 
 def data_generator():
     '''
     Returns augmented, labeled data
     '''
-    train_datagen = ImageDataGenerator(rescale= 1./255, validation_split = 0.2)
+    train_datagen = ImageDataGenerator(
+        rescale= 1./255, 
+        validation_split = 0.2,
+        vertical_flip = True,
+        horizontal_shift_range = 0.1,
+        shear_range = 0.2 
+        )
     valid_datagen = ImageDataGenerator(rescale= 1./255)
     test_datagen = ImageDataGenerator(rescale= 1./255)
 
@@ -19,7 +26,7 @@ def data_generator():
     valid_gen = valid_datagen.flow_from_directory(
         model_params['valid_dir'],
         target_size= (model_params['img_width'], model_params['img_height']), 
-        shuffle = False # change to False if using roc_plot
+        shuffle = True
     )
     test_gen = test_datagen.flow_from_directory(model_params['test_dir'])
     
@@ -47,7 +54,7 @@ def cnn_model():
     model.add(Dense(29)) 
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(3))
+    model.add(Dense(26))
     model.add(Activation('softmax'))
     model.compile(
         loss='categorical_crossentropy',
@@ -57,38 +64,17 @@ def cnn_model():
 
     return model
 
-def roc_plot():
-    '''
-    Returns a Receiver Operating Characterstic curve plot of model's performance
-    '''
-    valid_gen.reset()
-    x, classes = next(valid_gen)
-    preds = model.predict(x, verbose= 1)
-
-    fpr, tpr, _ = roc_curve(classes, preds)
-    roc_auc = auc(fpr, tpr)
-    plt.figure()
-    lw = 2
-    plt.plot(fpr, tpr, color= 'darkturquoise', lw= lw, label='ROC curve (area = %0.2f' % roc_auc)
-    plt.plot([0,1], [0,1], color='navy', lw= lw, linestyle= '--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Spiral Model Receiver Operating Characteristic')
-    plt.legend(loc= "lower right")
-    plt.show()
-
 def model_evaluation_plot():
     '''
     Returns plot of model's performance as measured by accuracy metrics
     '''
-    plt.plot(history.history['accuracy'], label= 'accuracy', color='lightgreen')
-    plt.plot(history.history['val_accuracy'], label= 'val_accuracy', color='lightgreen', linestyle= '--')
+    plt.plot(history.history['accuracy'], label= 'accuracy', color='yellowgreen')
+    plt.plot(history.history['val_accuracy'], label= 'val_accuracy', color='yellowgreen', linestyle= '--')
     plt.plot(history.history['loss'], label= 'loss', color= 'salmon')
     plt.plot(history.history['val_loss'], label= 'val_loss', color= 'salmon', linestyle= '--')
     plt.xlabel('Epoch')
     plt.ylim([0,1])
+    plt.title("Model Performance")
     plt.legend(loc='lower right')
     plt.show()
 
@@ -107,7 +93,7 @@ if __name__=='__main__':
         'batch_size': 50,
         'img_height': 200 ,
         'img_width': 200 ,
-        'epochs': 50 
+        'epochs': 100 
     }
 
     tensorboard_callback = TensorBoard(
@@ -122,11 +108,8 @@ if __name__=='__main__':
         )
     
     tensor_checkpoint = ModelCheckpoint('../data/models', save_best_only= True)
-
     model = cnn_model()
-
     train_datagen, valid_datagen, test_datagen, train_gen, valid_gen, test_gen = data_generator()
-
     model.summary()
 
     history = model.fit(
@@ -139,10 +122,7 @@ if __name__=='__main__':
     )
 
     model.evaluate(valid_gen)
-
-    roc_plot() # change validation shuffle to False if using roc_plot
-
+    print()
     model_evaluation_plot()
-
     model.save_weights('v1.h5')
 
