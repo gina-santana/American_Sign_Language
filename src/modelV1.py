5cc1c43b7ea810029758624e8c3bb675a5589967
Gina Santana <endernova@Ginas-MacBook-Pro.local>
2020-09-17 11:33:18 -0600

    Updated model

--- src/modelV1.py
+++ src/modelV1.py
@@ -6,9 +6,8 @@ from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
-from sklearn.metrics import plot_confusion_matrix
+from sklearn.metrics import plot_confusion_matrix, confusion_matrix, classification_report
 import numpy as np
-from skimage.color import rgb2gray
 from skimage.io import imread
 import os
 import PIL
@@ -48,11 +47,9 @@ def data_generator():
         shear_range = 0.1 
         )
     valid_datagen = ImageDataGenerator(rescale= 1./255)
-    # test_datagen = ImageDataGenerator(rescale= 1./255)
 
-    train_gen = train_datagen.flow_from_directory(model_params['train_dir'], target_size= (model_params['img_width'], model_params['img_height']), batch_size= model_params['batch_size'])
+    train_gen = train_datagen.flow_from_directory(model_params['train_dir'], target_size= (model_params['img_width'], model_params['img_height']), batch_size= model_params['batch_size']) #, shuffle=True)
     valid_gen = valid_datagen.flow_from_directory(model_params['valid_dir'], target_size= (model_params['img_width'], model_params['img_height']), batch_size=model_params['batch_size'])
-    # test_gen = test_datagen.flow_from_directory(model_params['test_dir'])
     
     return train_datagen, valid_datagen, train_gen, valid_gen
 
@@ -66,28 +63,34 @@ def cnn_model():
         input_shape = (model_params['img_width'], model_params['img_height'], 3)
     
     model = Sequential()
+
+    # model.add(Conv2D(128, (3,3), input_shape=input_shape))
+    # model.add(Activation('relu')) 
+    # model.add(MaxPooling2D(pool_size= (2,2))) 
+    # model.add(Dropout(0.2)) 
+
+    # model.add(Conv2D(64, (3,3)))
+    # model.add(Activation('relu')) 
+    # model.add(MaxPooling2D(pool_size= (2,2))) 
+    # model.add(Dropout(0.25)) 
+
     model.add(Conv2D(64, (3,3), input_shape=input_shape))
     model.add(Activation('relu')) 
     model.add(MaxPooling2D(pool_size= (2,2))) 
-    model.add(Dropout(0.2)) 
-    # for i, num_filters in enumerate(model_params['filters']):
-    #     if i == 0:
-    #         model.add(Conv2D(num_filters, (3,3), input_shape= input_shape))
-    #     else:
-    #         model.add(Conv2D(num_filters, (3,3), activation='relu'))
-    #     model.add(MaxPooling2D(pool_size=(2,2)))
-    #     model.add(Dropout(0.2))
-    model.add(Conv2D(128, (3,3)))
+    model.add(Dropout(0.25)) 
+
+    model.add(Conv2D(16, (3,3)))
     model.add(Activation('relu'))
-    # model.add(BatchNormalization())    
+    model.add(BatchNormalization())    
     model.add(MaxPooling2D(pool_size= (2,2)))
-    model.add(Dropout(0.2)) 
+    model.add(Dropout(0.3)) 
+
     model.add(Flatten())
-    # model.add(BatchNormalization())
-    model.add(Dense(250)) # consider this number - lower potentially
+    model.add(BatchNormalization())
+    model.add(Dense(80)) # consider this number - lower potentially # was 250
     model.add(Activation('relu'))
     model.add(Dropout(0.5)) 
-    # model.add(BatchNormalization())
+    model.add(BatchNormalization())
     model.add(Dense(28))
     model.add(Activation('softmax'))
     model.compile(
@@ -102,10 +105,10 @@ def model_evaluation_plot():
     '''
     Returns plot of model's performance as measured by accuracy metrics
     '''
-    plt.plot(history.history['accuracy'], label= 'accuracy', color='yellowgreen')
-    plt.plot(history.history['val_accuracy'], label= 'val_accuracy', color='yellowgreen', linestyle= '--')
-    plt.plot(history.history['loss'], label= 'loss', color= 'salmon')
-    plt.plot(history.history['val_loss'], label= 'val_loss', color= 'salmon', linestyle= '--')
+    plt.plot(history.history['accuracy'], label= 'accuracy', color='gray')
+    plt.plot(history.history['val_accuracy'], label= 'val_accuracy', color='orange', linestyle= '--')
+    plt.plot(history.history['loss'], label= 'loss', color= 'gray')
+    plt.plot(history.history['val_loss'], label= 'val_loss', color= 'orange', linestyle= '--')
     plt.xlabel('Epoch')
     plt.ylim([0,1])
     plt.title("Model Performance")
@@ -116,19 +119,24 @@ def confusion_matrix_plot():
     '''
     Returns confusion matrix of model's performance
     '''
-    
-    pass
+    pred_raw = model.predict(valid_gen)
+    pred = np.argmax(pred_raw)
+    print('Confusion Matrix')
+    print(confusion_matrix(valid_gen.classes, pred))
+    print('Classification Report')
+    target_names = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','space','nothing']
+    print(classification_report(valid_gen.classes, pred, target_names=target_names))
+
 
 if __name__=='__main__':
     model_params = {
         'train_dir': '../data/Train',
         'valid_dir': '../data/Valid',
         'test_dir': '../data/Test',
-        'filters': [16, 32, 64],
-        'batch_size': 50,
-        'img_height': 28, 
-        'img_width': 28,
-        'epochs': 30 
+        'batch_size': 40, # was 50
+        'img_height': 100, 
+        'img_width': 100,
+        'epochs': 15 
     }
     # print(img_to_array())
     search_and_destroy()
@@ -142,10 +150,10 @@ if __name__=='__main__':
         embeddings_freq= 0,
         embeddings_metadata= None
         )
-    early_stop = EarlyStopping(
-        monitor= 'val_loss'
-
-    )
+    # early_stop = EarlyStopping(
+    #     monitor= 'val_accuracy',
+    #     patience=
+    # )
     tensor_checkpoint = ModelCheckpoint('../data/models', save_best_only= True)
     model = cnn_model()
     train_datagen, valid_datagen, train_gen, valid_gen = data_generator()
@@ -156,10 +164,12 @@ if __name__=='__main__':
         steps_per_epoch = 58828 // model_params['batch_size'], # updates weight/backprop, can increase (more updates per epoch)
         epochs = model_params['epochs'],
         validation_data = valid_gen,
-        # validation_steps = 1, 
+        validation_steps = 1,
+        # validation_split= 0.3, 
         callbacks= [tensorboard_callback, tensor_checkpoint]
     )
 
     # model.evaluate(valid_gen)
     model_evaluation_plot()
+    # confusion_matrix_plot()
     model.save_weights('v1.h5')
