c76620d56b5f125e365c44ead5d0f6dfaa638a78
Gina Santana <endernova@Ginas-MacBook-Pro.local>
2020-09-20 11:39:02 -0600

    Updated model 1 code and add image of model 1 structure

--- src/modelV1.py
+++ src/modelV1.py
@@ -1,5 +1,6 @@
 from cnn_model import CNNModel
-from helper_functions import search_and_destroy
+from helper_functions import search_and_destroy 
+from keras.utils import plot_model
 
 model_params = {
     'train_dir': '../../../test2/Training',
@@ -16,10 +17,14 @@ model_params = {
 search_and_destroy()
 model = CNNModel(**model_params)
 model.fit()
+model.load_weights_from_file('../model_weights/v1rerun.h5')
+layers = model.trained_model.layers
 
 evaluated_validation = model.evaluate_model(model.create_generator(model.valid_dir, shuffle=False))
 model.print_classification_report(evaluated_validation)
 model.plot_confusion_matrix(**evaluated_validation)
+
+plot_model(model.trained_model, to_file='../images/V1structure.png')
 
 # model.save_weights('v1rerun.h5') #sobel_y
 # model.save_weights('v1rerun2.h5') # sobel_x
